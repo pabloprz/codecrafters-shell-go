9487585844ba28242ab310c5051aa9a7895cd147
Pablo Perez <pablo.perez.rodriguez@udc.es>
2024-12-03 15:55:34 -0600

    handling invalid commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,10 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	input := scanner.Text()
+	if len(input) != 0 {
+		input = strings.TrimSuffix(input, "\n")
+		fmt.Printf("%s: not found\n", input)
+	}
 }
