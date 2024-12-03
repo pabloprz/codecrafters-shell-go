4c44299edae9de15de05adb3b7760e6557d56cef
Pablo Perez <pablo.perez.rodriguez@udc.es>
2024-12-03 16:13:12 -0600

    handling exit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,9 +18,15 @@ func main() {
 			fmt.Println(err)
 			continue
 		}
-
-		fmt.Printf("%s: not found\n", input)
+		handleInput(input)
 	}
+}
+
+func handleInput(input string) {
+	if strings.HasPrefix(input, "exit") {
+		os.Exit(0)
+	}
+	fmt.Printf("%s: not found\n", input)
 }
 
 func readInput(scanner *bufio.Scanner) (string, error) {
